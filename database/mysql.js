da3b166d0125a415436f053493cfb94f503f3c61
doniiawan <doniiawan@gmail.com>
2021-04-11 23:41:54 +0700

    [feat] : add mysql db connector

---
+++ database/mysql.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql')
+const mysqlconn = mysql.createConnection({
+  host: process.env.MYSQL_HOST || 'localhost',
+  user: process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQL_PASS || 'root',
+  database: process.env.MYSQL_DATABASE || 'stockbit'
+})
+
+mysqlconn.connect()
+
+mysqlconn.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
+  if (err) throw err
+
+  console.log('SATU TAMBAH SATU YA: ', rows[0].solution)
+})
+
+// mysqlconn.end()
+
+module.exports = mysqlconn
