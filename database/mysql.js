2491bdb559e2f669c91c3a5cb22268d1981e5ea3
doniiawan <doniiawan@gmail.com>
2021-04-12 14:04:25 +0700

    [refactor] : remove unused lines

--- database/mysql.js
+++ database/mysql.js
@@ -3,7 +3,7 @@ const mysqlconn = mysql.createConnection({
   host: process.env.MYSQL_HOST || 'localhost',
   user: process.env.MYSQL_USER || 'root',
   password: process.env.MYSQL_PASS || 'root',
-  database: process.env.MYSQL_DATABASE || 'stockbit'
+  database: process.env.MYSQL_DATABASE || 'stockbit',
 })
 
 mysqlconn.connect()
@@ -11,9 +11,7 @@ mysqlconn.connect()
 mysqlconn.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
   if (err) throw err
 
-  console.log('SATU TAMBAH SATU YA: ', rows[0].solution)
+  console.log('DB Connected')
 })
-
-// mysqlconn.end()
 
 module.exports = mysqlconn
